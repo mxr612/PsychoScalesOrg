9356c13339d458e3b02d86ff9c99236022d93dce
mxr612 <academic@mxr612.io>
2025-03-05 10:08:57 +0000

    替换json为yml加载，简化了编辑。同时优化了数据结构

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 import markdown
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import json
+import json, yaml
 import os
 import uvicorn
 
@@ -16,11 +16,10 @@ def load_all_scales():
     scale_folder = 'scales'
     scales = {}
     for filename in os.listdir(scale_folder):
-        if filename.endswith('.json'):
+        if filename.endswith(('.yaml', '.yml')):
             with open(os.path.join(scale_folder, filename), 'r', encoding='utf-8') as f:
-                scale = json.load(f)
-                # 使用文件名作为问卷的唯一标识
-                scale_id = os.path.splitext(filename)[0]
+                scale = yaml.safe_load(f)
+                scale_id = os.path.splitext(filename)[0] # 使用文件名作为标识
                 scales[scale_id] = scale
     return scales
 
@@ -54,33 +53,27 @@ async def scale(request: Request, scale_id: str):
 
 @app.post("/result/{scale_id}", response_class=HTMLResponse)
 async def result(request: Request, scale_id: str):
-    # 保留原有的计分逻辑...
     form_data = await request.form()
-    # print(request.form)
     scales = load_all_scales()
     scale = scales.get(scale_id)
     if scale:
+        # 这里可以添加保存数据到数据库等逻辑
         responses = {}
         ranges = {}
-        # print(scale['questions'])
-        for question in scale['questions']:
-            # print(question['subscale'])
-            if question['subscale'] not in responses:
-                responses[question['subscale']] = 0
-                ranges[question['subscale']] = [0,0]
-            if 'reverse' in question and question['reverse']:
-                responses[question['subscale']] += question['range'][1] + question['range'][0] - int( form_data[question['id']])
-            else:
-                responses[question['subscale']] += int( form_data[question['id']])
-            ranges[question['subscale']][0] += question['range'][0]
-            ranges[question['subscale']][1] += question['range'][1]
-        # 这里可以添加保存数据到数据库等逻辑
-        # print(ranges)
+        for subscale, qids in scale['subscales'].items():
+            responses[subscale] = 0
+            ranges[subscale] = [len(scale['range'][0]*qids),len(scale['range'][1]*qids)]
+            for qid in qids:
+                print(qid)
+                if qid<0:
+                    responses[subscale] += scale['range'][0] + scale['range'][1] - int(form_data[str(-qid)])
+                else:
+                    responses[subscale] += int(form_data[str(qid)])
         return templates.TemplateResponse("result.html", {
             "request": request,
             "responses": responses,
             "ranges": ranges,
-            "scale_title": scale['title']
+            "scale": scale
         })
     raise HTTPException(status_code=404, detail="问卷未找到")
 
